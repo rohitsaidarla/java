27603171c848fd6c9effd8a8d19c5af9bbae7f51
rohitdarla <155637569+rohitsaidarla@users.noreply.github.com>
2024-05-30 16:22:29 +0530

    Add files via upload

---
+++ election/src/componets/Header.js
@@ -0,0 +1,23 @@
+const Header = () => {
+
+    return(
+      <div className="header">
+        <div className="logo">
+        <img src="https://t4.ftcdn.net/jpg/05/84/26/61/360_F_584266196_lFkZt7CCgGrlhil83DsI0MyGvd8eSDoc.png "/>
+        </div>
+        
+        <h1>ONLINE ELECTION SYSTEM</h1>
+        <div className="button">
+        <button>HOME</button><button>LOGOUT</button>
+        
+        </div>
+      
+      </div>
+       
+        
+
+
+
+    );
+}
+export default Header;
